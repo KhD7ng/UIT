83a82616c0c71a06752bf87a65c8f9109306a518
Trần Đình Khánh Đăng <khanhdangtran2004@gmail.com>
2023-10-21 21:40:09 +0700

    Lab2

--- IT004 - CSDL/22520195_Lab2.sql
+++ IT004 - CSDL/22520195_Lab2.sql
@@ -53,7 +53,7 @@ CREATE TABLE THANNHAN (
 	MA_NVIEN nvarchar(9),
 	TENTN nvarchar(15),
 	PHAI nvarchar(3),
-	NGSINH date,
+	NGSINH datetime,
 	QUANHE nvarchar(15)
 	PRIMARY KEY (MA_NVIEN, TENTN)
 )
@@ -73,6 +73,8 @@ INSERT INTO PHONGBAN VALUES(N'Nghiên cứu', 5, '005', '22/05/1978')
 INSERT INTO PHONGBAN VALUES(N'Điều hành', 4, '008', '01/01/1985')
 INSERT INTO PHONGBAN VALUES(N'Quản lý', 1, '006', '19/06/1971')
 
+DROP TABLE THANNHAN
+SET DATEFORMAT DMY
 INSERT INTO THANNHAN VALUES('005', N'Trinh', N'Nữ', '05/04/1976', N'Con gái')
 INSERT INTO THANNHAN VALUES('005', N'Khang', N'Nam', '25/10/1973', N'Con trai')
 INSERT INTO THANNHAN VALUES('005', N'Phương', N'Nữ', '03/05/1948', N'Vợ chồng')
@@ -121,34 +123,31 @@ INSERT INTO CONGVIEC VALUES(20, 1, 'Lap dat cap quang')
 INSERT INTO CONGVIEC VALUES(30, 1, 'Dao tao nhan vien Marketing')
 INSERT INTO CONGVIEC VALUES(30, 2, 'Dao tao chuyen vien vien thiet ke')
 
-SELECT * FROM NHANVIEN
-SELECT * FROM PHONGBAN
-SELECT * FROM THANNHAN
---1
+--1.	Tìm các nhân viên làm việc ở phòng số 4
 SELECT * FROM NHANVIEN WHERE PHG = 4
---2
+--2.	Tìm các nhân viên có mức lương trên 30000
 SELECT * FROM NHANVIEN WHERE LUONG > 30000
---3
+--3.	Tìm các nhân viên có mức lương trên 25,000 ở phòng 4 hoặc các nhân viên có mức lương trên 30,000 ở phòng 5
 SELECT * FROM NHANVIEN WHERE (PHG = 4 AND LUONG > 25000) OR (PHG = 5 AND LUONG > 30000)
---4
+--4.	Cho biết họ tên đầy đủ của các nhân viên ở TP HCM
 SELECT HONV + ' ' + TENLOT + ' ' + TENV FROM NHANVIEN WHERE DCHI LIKE '%Tp HCM'
---5
+--5.	Cho biết họ tên đầy đủ của các nhân viên có họ bắt đầu bằng ký tự 'N'
 SELECT HONV + ' ' + TENLOT + ' ' + TENV FROM NHANVIEN WHERE HONV LIKE 'N%'
---6
+--6.	Cho biết các nhân viên sinh từ năm 1960 đến 1965
 SELECT * FROM NHANVIEN WHERE YEAR(NGSINH) BETWEEN 1960 AND 1965
---7
+--7.	Cho biết nhân viên và năm sinh của nhân viên
 SELECT HONV, TENLOT, TENV, YEAR(NGSINH) NAMSINH FROM NHANVIEN
---8
+--8.	Cho biết nhân viên và tuổi của nhân viên 
 SELECT HONV, TENLOT, TENV, DATEDIFF(YYYY, NGSINH, GETDATE()) TUOI FROM NHANVIEN
---9
+--9.	Với mỗi phòng ban, cho biết tên phòng ban và địa điểm phòng
 SELECT TENPHG, DIADIEM FROM	PHONGBAN PB, DIADIEM_PHG DDP WHERE PB.MAPHG = DDP.MAPHG
---10
+--10.	Tìm tên những người trưởng phòng của từng phòng ban
 SELECT HONV, TENLOT, TENV FROM NHANVIEN NV, PHONGBAN PB WHERE NV.MANV = PB.TRPHG
---11
+--11.	Tìm tên và địa chỉ của tất cả các nhân viên của phòng "Nghiên cứu".
 SELECT TENV, DCHI FROM NHANVIEN NV, PHONGBAN PB WHERE NV.PHG = PB.MAPHG AND PB.TENPHG = N'Nghiên cứu'
---12
---SELECT TENDA, PHONG
---13
---SELECT TENV, 
---14
+--12.	Với mỗi đề án ở Hà Nội, cho biết tên đề án, tên phòng ban, họ tên và ngày nhận chức của trưởng phòng của phòng ban chủ trì đề án đó.
+SELECT TENDA, TENPHG, TENV, NG_NHANCHUC FROM PHONGBAN PB, NHANVIEN NV, DEAN DA WHERE NV.MANV = PB.TRPHG AND DA.PHONG = PB.MAPHG AND DA.DDIEM_DA LIKE N'%Hà Nội'
+--13.	Với mỗi nhân viên, cho biết tên nhân viên và tên người quản lý trực tiếp của nhân viên đó
+SELECT A.TENV, B.TENV FROM NHANVIEN A , NHANVIEN B WHERE A.MA_NQL = B.MANV
+--14.	Tìm tên những nữ nhân viên và tên người thân của họ
 SELECT TENV, TENTN FROM NHANVIEN NV, THANNHAN TN WHERE NV.MANV = TN.MA_NVIEN AND NV.PHAI = N'Nữ'
